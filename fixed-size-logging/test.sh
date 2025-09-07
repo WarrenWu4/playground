1665c15b41b218488830fa2d5a8b2c3647824453
WarrenWu4 <warrenweiwu04@gmail.com>
2025-09-07 17:24:37 -0500

    fix: this makes more sense

--- fixed-size-logging/test.sh
+++ fixed-size-logging/test.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-msg=(100, 10000, 100000)
 kb=(100, 1000, 10000)
+msg=(100, 10000, 100000)
 
 command="make run"
 
 for ((i=0; i<3; i++)); do
-  arg1="${msg[$i]}"
-  echo "Starting num_messages=${arg1}"
+  arg1="${kb[$i]}"
+  echo "Starting kb=${arg1}"
   echo "--------------------------------"
   for ((j=0; j<3; j++)); do
-    arg2="${kb[$j]}"
-    echo "Starting kb=${arg2}"
+    arg2="${msg[$j]}"
+    echo "Starting msg=${arg2}"
     output_file="tests/out_${arg1}_${arg2}.txt"
     eval "${command} MESSAGES=${arg1} KB=${arg2}" > "${output_file}"
   done
