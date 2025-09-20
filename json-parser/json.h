12b07f4a2d2ec9cf357999deea0d766a04268189
WarrenWu4 <warrenweiwu04@gmail.com>
2025-09-20 16:54:01 -0500

    feat: rewrote json library

--- json-parser/json.h
+++ json-parser/json.h
@@ -1,85 +1,213 @@
 #pragma once
 
 #include <any>
+#include <cctype>
 #include <cstddef>
 #include <fstream>
 #include <iostream>
+#include <istream>
+#include <iterator>
 #include <string>
 #include <unordered_map>
 #include <vector>
 #include <regex>
+#include <variant>
+#include <assert.h>
+
+struct JsonValue;
+using JsonArray = std::vector<JsonValue>;
+using JsonObject = std::map<std::string, JsonValue>;
+using JsonType = std::variant<
+    std::nullptr_t,
+    bool,
+    double,
+    std::string,
+    JsonArray,
+    JsonObject
+>;
+
+struct JsonValue {
+    JsonType value;
+    bool isNull() const { return std::holds_alternative<std::nullptr_t>(value); }
+    bool isBool() const { return std::holds_alternative<bool>(value); }
+    bool isNumber() const { return std::holds_alternative<double>(value); }
+    bool isString() const { return std::holds_alternative<std::string>(value); }
+    bool isArray() const { return std::holds_alternative<JsonArray>(value); }
+    bool isObject() const { return std::holds_alternative<JsonObject>(value); }
+    bool getBool() const {
+        if (!isBool()) throw std::runtime_error("Type is not bool");
+        return std::get<bool>(value);
+    }
+    double getNumber() const {
+        if (!isNumber()) throw std::runtime_error("Type is not number");
+        return std::get<double>(value);
+    }
+    const std::string& getString() const {
+        if (!isString()) throw std::runtime_error("Type is not string");
+        return std::get<std::string>(value);
+    }
+    const JsonArray& getArray() const {
+        if (!isArray()) throw std::runtime_error("Type is not array");
+        return std::get<JsonArray>(value);
+    }
+    const JsonObject& getObject() const {
+        if (!isObject()) throw std::runtime_error("Type is not object");
+        return std::get<JsonObject>(value);
+    }
+};
 
 class Json {
 private:
-  std::unordered_map<std::string, std::any> data;
+    std::string data;
+    size_t end;
+    size_t ptr;
 
-
-  void removeAllWhitespaces(std::string& str) {
-    size_t writePtr = 0;
-    for (size_t i = 0; i < str.size(); i++) {
-      if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n') { continue; } 
-      str[writePtr] = str[i];
-      writePtr += 1;
+    void skipWhitespace() {
+        while (ptr < end && std::isspace(data[ptr])) { ptr++; }
     }
-    for (size_t i = writePtr; i < str.size(); i++) {
-      str[i] = ' ';
-    }
-  }
 
-  void parseData(std::string_view processedData) {
-    std::regex integerType("^[0-9]+$");
-    std::regex doubleType("^[0-9]+(\\.[0-9]+)?$");
-    std::string key = "";
-    std::string value = "";
-    bool isKey = true;
-    for (size_t i = 0; i < processedData.size(); i++) {
-      if (processedData[i] == '"') { continue; }
-      if (processedData[i] == '{' || processedData[i] == '}') {
-        if (key != "" && value != "") {
-          data[key] = value;
+    JsonValue parseValue() {
+        skipWhitespace();
+        assert(ptr < end);
+        switch (data[ptr]) {
+            case 'n': { return parseNull(); }
+            case 't': case 'f': { return parseBool(); }
+            case '-': case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9': { return parseNumber(); }
+            case '"': { return parseString(); }
+            case '[': { return parseArray(); }
+            case '{': { return parseObject(); } 
+            default: {
+                throw std::runtime_error("Unexpected character");
+            }
         }
-      } else if (processedData[i] == ',') {
-        isKey = true;
-        if (std::regex_match(value, integerType)) {
-          data[key] = std::stoi(value);
-        } else if (std::regex_match(value, doubleType)) {
-          data[key] = std::stod(value);
-        } else {
-          data[key] = value;
-        }
-        key = "";
-        value = "";
-      } else if (processedData[i] == ':') {
-        isKey = false;
-      } else if (isKey) {
-        key += processedData[i];
-      } else {
-        value += processedData[i];
-      }
     }
-  }
+    
+    JsonValue parseNull() {
+        assert(ptr < end);
+        if (data.substr(ptr, 4) == "null") { ptr += 4; return {nullptr}; }
+        throw std::runtime_error("Cannot parse null value");
+    }
+
+    JsonValue parseBool() {
+        assert(ptr < end);
+        if (data.substr(ptr, 4) == "true") { ptr += 4; return {true}; }
+        if (data.substr(ptr, 5) == "false") { ptr += 5; return {false}; }
+        throw std::runtime_error("Cannot parse boolean value");
+    }
+
+    JsonValue parseNumber() {
+        size_t start = ptr;
+        while (ptr < data.length() && (isdigit(static_cast<unsigned char>(data[ptr])) || data[ptr] == '.' || data[ptr] == '-' || data[ptr] == 'e' || data[ptr] == 'E')) {
+            ptr++;
+        }
+        return {std::stod(data.substr(start, ptr - start))};
+    }
+
+    JsonValue parseString() {
+        ptr++;
+        std::string result = "";
+        while (ptr < data.length() && data[ptr] != '"') {
+            if (data[ptr] == '\\') {
+                ptr++;
+                if (ptr >= data.length()) {
+                    throw std::runtime_error("Invalid escape sequence");
+                }
+                switch (data[ptr]) {
+                    case '"': result += '"'; break;
+                    case '\\': result += '\\'; break;
+                    case '/': result += '/'; break;
+                    case 'b': result += '\b'; break;
+                    case 'f': result += '\f'; break;
+                    case 'n': result += '\n'; break;
+                    case 'r': result += '\r'; break;
+                    case 't': result += '\t'; break;
+                    default: throw std::runtime_error("Invalid escape sequence");
+                }
+            } else {
+                result += data[ptr];
+            }
+            ptr++;
+        }
+        if (ptr >= data.length()) {
+            throw std::runtime_error("Unterminated string literal");
+        }
+        ptr++; // Skip final '"'
+        return {result};
+    }
+
+    JsonValue parseArray() {
+        ptr++;
+        skipWhitespace();
+        JsonArray arr;
+        if (data[ptr] == ']') {
+            ptr++;
+            return {arr};
+        }
+        while (true) {
+            arr.push_back(parseValue());
+            skipWhitespace();
+            if (data[ptr] == ']') {
+                ptr++;
+                break;
+            } else if (data[ptr] != ',') {
+                throw std::runtime_error("Expected ',' or ']'");
+            }
+            ptr++;
+        }
+        return {arr};
+    }
+
+    JsonValue parseObject() {
+        ptr++;
+        skipWhitespace();
+        JsonObject obj;
+        if (data[ptr] == '}') {
+            ptr++;
+            return {obj};
+        }
+        while (true) {
+            std::string key = std::get<std::string>(parseString().value);
+            skipWhitespace();
+            if (data[ptr] != ':') { throw std::runtime_error("Expected ':'"); }
+            ptr++;
+            skipWhitespace();
+            obj[key] = parseValue();
+            skipWhitespace();
+            if (data[ptr] == '}') {
+                ptr++;
+                break;
+            } else if (data[ptr] != ',') {
+                throw std::runtime_error("Expected ',' or '}'");
+            }
+            ptr++;
+            skipWhitespace();
+        }
+        return {obj};
+    }
+
+    std::string readFile(std::string path) {
+        std::ifstream file(path.c_str());
+        if (!file.is_open()) {
+            throw std::runtime_error("Unable to open file");
+        }
+        std::string res(
+            (std::istreambuf_iterator<char>(file)),
+            std::istreambuf_iterator<char>()
+        );
+        return res;
+    }
 
 public:
-  Json() {}
-  ~Json() {}
-  std::unordered_map<std::string, std::any> parse(std::string_view path) {
-    std::string rawData;
-    readData(path, rawData);
-    removeAllWhitespaces(rawData);
-    parseData(rawData);
-    return data;
-  }
-  void readData(std::string_view path, std::string &rawData) {
-    std::ifstream f;
-    f.open(path.data(), std::ios::in);
-    if (!f.is_open()) {
-      std::cerr << "Unable to open file\n";
-      return;
+    JsonValue parse(std::string text) {
+        data = text;
+        end = data.size();
+        ptr = 0;
+        return parseValue();
     }
-    std::string temp;
-    while (std::getline(f, temp)) {
-      rawData += temp;
+    JsonValue parseFromFile(std::string path) {
+        data = readFile(path);
+        end = data.size();
+        ptr = 0;
+        return parseValue();
     }
-    f.close();
-  }
 };
--- json-parser/main.cpp
+++ json-parser/main.cpp
@@ -1,20 +1,14 @@
 #include "json.h"
-#include "jsonge.h"
-#include <exception>
 #include <string>
 
+struct Settings {
+};
+
 int main() {
-  Json j = Json();
-  std::string path = "test.json";
-  std::string data;
-  j.readData(path, data);
-  try {
-    std::cout << "Starting\n";
-    json::Value root = json::parse(data);
-    std::cout << root["test"].asInt() << "\n";
-    std::cout << json::stringify(root) << "\n";
-  } catch (const std::exception &e) {
-    std::cerr << "Error: " << e.what() << "\n";
-  }
-  return 0;
+    Json j = Json();
+    std::string path = "test.json";
+    JsonValue res = j.parseFromFile(path);
+    JsonObject obj = res.getObject();
+    std::cout << obj.at("textColor").getString();
+    return 0;
 }
